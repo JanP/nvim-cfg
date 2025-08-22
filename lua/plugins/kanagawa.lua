603294af08f01d4ca63fbc4f64aaf33276a2f239
Jan Paesmans <jan.paesmans@gmail.com>
2025-08-22 21:52:45 +0200

    Reindent

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,24 +1,36 @@
 return {
-	"rebelot/kanagawa.nvim",
-	branch = master,
-	opts = function()
-		require('kanagawa').setup({
-			compile = true,
-			overrides = function(colors)
-				return {
-					["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
-					["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
-					["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
-					["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
-					["@markup.list.markdown"] = { link = "Function" }, -- + list
-					["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
-					["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- - [X] checked list item
-				}
-			end
-		});
-		vim.cmd("colorscheme kanagawa");
-	end,
-	build = function()
-		vim.cmd("KanagawaCompile");
-	end
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require("kanagawa").setup({
+            compile = true,
+            transparent = true,
+            overrides = function(colors)
+                return {
+                    ["@markup.link.url.markdown_inline"] = { -- (url)
+                        link = "Special",
+                    },
+                    ["@markup.link.label.markdown_inline"] = { -- [label]
+                        link = "WarningMsg",
+                    },
+                    ["@markup.italic.markdown_inline"] = { -- *italic*
+                        link = "Exception",
+                    },
+                    ["@markup.raw.markdown_inline"] = { -- `code`
+                        link = "String",
+                    },
+                    ["@markup.list.markdown"] = { -- + list
+                        link = "Function",
+                    },
+                    ["@markup.quote.markdown"] = { -- > blockcode
+                        link = "Error",
+                    },
+                    ["@markup.list.checked.markdown"] = { -- - [x] checked list item
+                        link = "WarningMsg",
+                    },
+                }
+            end,
+        })
+        vim.cmd("colorscheme kanagawa")
+    end,
+    build = function() vim.cmd("KanagawaCompile") end,
 }
